793b0d14c20f9e10dab45439b416eaf4bee5ba41
ormsng <ormessing96@gmail.com>
2023-05-08 17:09:06 +0300

    Add tests

---
+++ pages/customerLoginPage.py
@@ -0,0 +1,17 @@
+
+from tests import customerLoginButtonStepOne
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from tests import *
+
+
+driver = webdriver.Chrome()
+driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login")
+
+driver.implicitly_wait(20) # gives an implicit wait for 20 seconds
+
+loginButton = driver.find_element(By.XPATH, '/html/body/div/div/div[2]/div/div[1]/div[1]/button')
+customerLoginButtonStepOne()
+
+driver.close()
